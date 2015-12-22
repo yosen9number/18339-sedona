0aed6430c26fc40036f8d0ec8ffcbc6aad704081
yosen9number <korosu.o.korosu@gmail.com>
2015-12-22 15:10:20 +0300

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
           src: [
             "img/**",
             "js/**",
-            "index.html"
+            "*.html"
           ],
           dest: "build"
         }]
